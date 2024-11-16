da6b1606f4718cc12cc7703dd51dcf755651feea
Satyam Kumar Verman <satyamvarman2004@gmail.com>
2024-11-16 17:33:56 +0530

    Update wsgi.py

--- sixteen/wsgi.py
+++ sixteen/wsgi.py
@@ -1,7 +1,7 @@
 """
 WSGI config for sixteen project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+It exposes the WSGI callable as a module-level variable named ``app``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
@@ -13,4 +13,4 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sixteen.settings')
 
-application = get_wsgi_application()
+app = get_wsgi_application()
